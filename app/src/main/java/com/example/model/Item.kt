b02e871deb3c61b830413cce2b779e50c25bc67a
Alexandre <alexandre.leroyer@esme.fr>
2021-04-27 19:55:59 +0200

    classe

---
+++ app/src/main/java/com/example/model/Item.kt
@@ -0,0 +1,13 @@
+package com.example.model
+
+class Item {
+
+    var type = ""
+    var nom = ""
+    var qt = 0
+
+    class Item constructor(val type: String, val nom: String, val qt: Int){
+
+
+    }
+}
