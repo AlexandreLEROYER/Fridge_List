7463f194d8a1797e418a6be9951cdb57fca3152b
Maxime <2000max.char@laposte.net>
2021-05-01 18:56:36 +0200

    Voilà enfin les 4 je suis fier des 4

--- app/src/main/java/com/example/fridge_list/MainActivity.kt
+++ app/src/main/java/com/example/fridge_list/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.comparaison)
+        setContentView(R.layout.action_de_liste)
     }
 }
