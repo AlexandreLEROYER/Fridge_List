02d2b20930effdc14fbb84aa8a61531dc4351137
Alexandre <alexandre.leroyer@esme.fr>
2021-05-02 19:08:23 +0200

    classe

---
+++ app/src/main/java/com/example/fridge_list/ListActivity.kt
@@ -0,0 +1,27 @@
+package com.example.fridge_list
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
+import android.widget.TextView
+
+class ListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.liste)
+
+        val returnMenu : ImageButton = findViewById(R.id.floatingActionButton2)
+        returnMenu.setOnClickListener {
+            val mainIntent : Intent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+            Log.d("TAG", "FrigoAct")
+        }
+
+        val name = intent.getStringExtra(EXTRA_NAME)
+        val titleList = findViewById<TextView>(R.id.textView2).apply {
+            text = name
+        }
+    }
+}
--- app/src/main/java/com/example/fridge_list/MainActivity.kt
+++ app/src/main/java/com/example/fridge_list/MainActivity.kt
@@ -1,12 +1,63 @@
 package com.example.fridge_list
 
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
+import android.util.Log
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+
+const val EXTRA_NAME = "com.example.fridge_list.NAME"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.menu)
+
+        val viewMenu : ImageButton = findViewById(R.id.imageButton3)
+        viewMenu.setOnClickListener {
+            val frigoIntent : Intent = Intent(this, FrigoActivity::class.java)
+            startActivity(frigoIntent)
+            Log.d("TAG", "FrigoAct")
+        }
+
+        val viewList : ImageButton = findViewById(R.id.floatingActionButton)
+        viewList.setOnClickListener {
+            val nameList : AlertDialog.Builder = AlertDialog.Builder(this)
+            nameList.setTitle("Nom de la nouvelle liste")
+            nameList.setMessage("Rentrez le nom de votre nouvelle liste")
+
+            val nameField : EditText = EditText(this)
+            nameField.hint = "Nouvelle Liste"
+            nameField.inputType = InputType.TYPE_CLASS_TEXT
+            nameList.setView(nameField)
+
+            nameList.setPositiveButton("Confirmer", DialogInterface.OnClickListener { dialog, which ->
+                var name = nameField.text.toString()
+
+                if (name == "") {
+                    Toast.makeText(applicationContext, "Il faut donner un nom gros", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    val listIntent : Intent = Intent(this, ListActivity::class.java).apply {
+                        putExtra(EXTRA_NAME, name)
+                    }
+                    startActivity(listIntent)
+                    Toast.makeText(applicationContext, "Liste créée bg", Toast.LENGTH_SHORT).show()
+                }
+            })
+            nameList.setNegativeButton("Annuler", DialogInterface.OnClickListener { dialog, which ->
+                Toast.makeText(applicationContext, "Dommage", Toast.LENGTH_SHORT).show()
+            })
+            nameList.show()
+
+            Log.d("TAG", "ListAct")
+        }
     }
+
 }
